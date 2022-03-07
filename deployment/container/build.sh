2caf70b8e28ed2f614775f8905b29405a04be1b2
Joe Wood <joe.dean.wood@gmail.com>
2022-03-07 14:26:53 -0500

    update container build script

--- deployment/container/build.sh
+++ deployment/container/build.sh
@@ -1,3 +1,7 @@
 # build file for Docker container
-docker build -t liatrio-api .
-docker run --name liatrio-api -p 5000:5000 -d liatrio-api
+NAME="liatrio-api"
+VER="1.0.0"
+REPO="woejood"
+
+docker build ../../app/ -t liatrio-api:1.0.0 --label liatrio-api:1.0.0
+docker run --name liatrio-api:1.0.0 -p 5000:5000 -d liatrio-api:1.0.0
