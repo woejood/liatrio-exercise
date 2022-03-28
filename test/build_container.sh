be84639c9de257891b40ffb0fd26bdc72e3fa646
Joe Wood <joe.dean.wood@gmail.com>
2022-03-28 12:20:10 -0400

    updated build script

--- test/build_container.sh
+++ test/build_container.sh
@@ -4,10 +4,8 @@
 NAME="woejood/liatrio-api"
 TAG="1.0.0"
 
-docker build ../ -t $NAME:$TAG #--label $NAME:$TAG
+docker build ../ -t $NAME:$TAG --label $NAME:$TAG
 
 docker tag $NAME:$TAG "${NAME}:latest"
 docker push $NAME:$TAG
 docker push "${NAME}:latest"
-
-#docker run --name $NAME:$TAG -p 5000:5000 -d $NAME:$TAG
